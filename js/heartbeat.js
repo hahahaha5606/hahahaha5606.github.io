11c057f03a063a5530ecd590ef7c564a3c314136
hahahaha5606 <guo.@yali.github.com>
2024-01-29 21:39:34 +0800

    Site updated: 2024-01-29 21:39:33

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -1,6 +1,6 @@
 // 动态心跳
 $(document).ready(function(e){
-    $('.copyright').html('<i style="color:#FF6A6A">©2023</i> <i style="color:#FF6A6A" class="fa-fw fas fa-heartbeat card-announcement-animation cc_pointer"></i> By 胖胖大藕片');
+    $('.copyright').html('<i style="color:#b3c9d0">©2023</i>  <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> <i style="color:#b3c9d0">By 胖胖大藕片</i>');
 })
 
 $(document).ready(function(e){
