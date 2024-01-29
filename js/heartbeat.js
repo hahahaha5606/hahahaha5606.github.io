beded5121171b50513b481f76fcf53f3431373a4
hahahaha5606 <guo.@yali.github.com>
2024-01-29 21:43:56 +0800

    Site updated: 2024-01-29 21:43:55

--- js/heartbeat.js
+++ js/heartbeat.js
@@ -9,7 +9,7 @@ $(document).ready(function(e){
 
 //本站运行时间
 function show_date_time(){
-$('.framework-info').html('本站已运行<span id="span_dt_dt" style="color: #b3c9d0;"></span>');
+$('.framework-info').html('<i style="color:#b3c9d0">本站已运行</i> <span id="span_dt_dt" style="color: #b3c9d0;"></span>');
 window.setTimeout("show_date_time()", 1000);
 BirthDay=new Date("11/6/2023 0:0:0");
 today=new Date();
@@ -24,6 +24,6 @@ hrsold=Math.floor(e_hrsold);
 e_minsold=(e_hrsold-hrsold)*60;
 minsold=Math.floor((e_hrsold-hrsold)*60);
 seconds=Math.floor((e_minsold-minsold)*60);
-span_dt_dt.innerHTML=' <font style=color:green>'+daysold+'</font> 天 <font style=color:#f391a9>'+hrsold+'</font> 时 <font style=color:#fdb933>'+minsold+'</font> 分 <font style=color:#a3cf62>'+seconds+'</font> 秒';
+span_dt_dt.innerHTML=' <font style=color:afb4db>'+daysold+'</font> 天 <font style=color:#f391a9>'+hrsold+'</font> 时 <font style=color:#fdb933>'+minsold+'</font> 分 <font style=color:#a3cf62>'+seconds+'</font> 秒';
 }
 
