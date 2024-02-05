3065704f810154e2923e950ff84963ba73cbcd11
hahahaha5606 <guo.@yali.github.com>
2024-02-05 09:04:11 +0800

    Site updated: 2024-02-05 09:04:10

--- js/fish.js
+++ js/fish.js
@@ -166,7 +166,7 @@ var RENDERER = {
 		requestAnimationFrame(this.render);
 		this.controlStatus();
 		this.context.clearRect(0, 0, this.width, this.height);
-		this.context.fillStyle = 'hsl(191,47%,50%)';
+		this.context.fillStyle = 'hsl(184,68%,49%)';
 		
 		for(var i = 0, count = this.fishes.length; i < count; i++){
 			this.fishes[i].render(this.context);
